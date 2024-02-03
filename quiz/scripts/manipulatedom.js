01abc41eb7db9f6d7b8a62a280637ce4cb9874f0
Rahul Suresh <rahs98@gmail.com>
2024-02-02 21:29:08 -0500

    Class todos

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -1,31 +1,44 @@
-window.onload = function() {
-  document.getElementById('addTableBtn').addEventListener('click', addTable);
-}
+window.onload = function () {
+    document.getElementById("addTableBtn").addEventListener("click", addTable);
+};
 
 function createTRNode(colNodes) {
-  let trNode = document.createElement("tr");
-  colNodes.forEach(function(colNode) {
-    trNode.appendChild(colNode);
-  })
-  return trNode;
+    let trNode = document.createElement("tr");
+    colNodes.forEach(function (colNode) {
+        trNode.appendChild(colNode);
+    });
+    return trNode;
 }
 
 function createTDNode(childNode) {
-  let tdNode = document.createElement("td");
-  tdNode.appendChild(childNode);
-  return tdNode;
+    let tdNode = document.createElement("td");
+    tdNode.appendChild(childNode);
+    return tdNode;
 }
 
 function createTxtNode(txt) {
-  let txtNode = document.createTextNode(txt);
-  return txtNode;
+    let txtNode = document.createTextNode(txt);
+    return txtNode;
+}
+
+function createBtnNode(btnText, btnTarget) {
+    let btnNode = document.createElement("button");
+    let textInput = document.createElement("input");
+    textInput.setAttribute("placeholder", "Enter Cell (x,y) ...");
+    btnNode.textContent = btnText;
+    btnNode.addEventListener("click", () =>
+        document.getElementById(btnTarget).replaceWith(textInput)
+    );
+    return btnNode;
 }
 
 function addTable() {
-  const tableNode = document.createElement("table");
-  for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
-  }
-  document.getElementById("root").appendChild(tableNode);
+    const tableNode = document.createElement("table");
+    for (let i = 0; i < 3; i++) {
+        let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
+        col1.setAttribute("id", `cell-${i}`);
+        let col2 = createTDNode(createBtnNode("Edit text", `cell-${i}`));
+        tableNode.appendChild(createTRNode([col1, col2]));
+    }
+    document.getElementById("root").appendChild(tableNode);
 }
--- quiz/scripts/map-deep.js
+++ quiz/scripts/map-deep.js
@@ -2,17 +2,17 @@
 const originalMatrix = [
     [3, 2, 1],
     [4, 5, 6],
-    [7, 9, 8]
-  ];
-  
-  const newMatrix = originalMatrix.map((row) => {
-    return row.sort();
-  });
-  
-  // Modifying the original matrix (changing the last element of the first row)
-  originalMatrix[0][2] = 99;
-  
-  // Displaying the original and new matrices
-  console.log('Original Matrix (modified):', originalMatrix);
-  console.log('New Matrix (unaffected):', newMatrix);
-  
+    [7, 9, 8],
+];
+
+const newMatrix = originalMatrix.map((row) => {
+    new_row = [...row];
+    return new_row.sort();
+});
+
+// Modifying the original matrix (changing the last element of the first row)
+originalMatrix[0][2] = 99;
+
+// Displaying the original and new matrices
+console.log("Original Matrix (modified):", originalMatrix);
+console.log("New Matrix (unaffected):", newMatrix);
