ef1d01d351d11450bbdfd95ee386d2d3cb899530
Rahul Suresh <rahs98@gmail.com>
2024-02-06 16:52:07 -0500

    Class work

--- quiz/scripts/stack-q1.js
+++ quiz/scripts/stack-q1.js
@@ -1,33 +1,43 @@
 class PStack {
-  #id;
-  constructor() {
-    this.#id = 1;
-    this._persons = [];
-  }
+    #id;
+    constructor() {
+        this.#id = 1;
+        this._persons = [];
+    }
 
-  showId() {
-    return this.#id;
-  }
-
+    showId() {
+        return this.#id;
+    }
 }
 
 class PStackImpl extends PStack {
-  constructor() {
-    super();
-  }
+    constructor() {
+        super();
+    }
 
-  push(p) {
-    return this._persons.push(p)
-  }
+    get persons() {
+        return this._persons;
+    }
 
-  pop() {
-    return this._persons.pop().age
-  }
+    set persons(persons) {
+        this._persons = persons;
+    }
+
+    push(p) {
+        return this._persons.push(p);
+    }
+
+    pop() {
+        return this._persons.pop().age;
+    }
 }
 
 let pstack = new PStackImpl();
-pstack.persons = [{name: 'Jojo', age: 21}, {name: 'Gabi', age: 29}]
-pstack.push({name: 'Dein', age: 19});
+pstack.persons = [
+    { name: "Jojo", age: 21 },
+    { name: "Gabi", age: 29 },
+];
+pstack.push({ name: "Dein", age: 19 });
 console.log(pstack.pop());
 console.log(pstack.pop());
 console.log(pstack.persons);
